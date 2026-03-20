8bb73cd42943064c8472957a282375306df452fd
szanicsferenc <szanicsferenc08@gmail.com>
2026-03-20 09:51:14 +0100

    alapok letétele és feladat

---
+++ nyilvantartas/nyilvantartas/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace nyilvantartas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingatlan adatai: Cím, helyrajzi szám, méret ($m ^ 2$).
+            //Bérlő adatai: Név, szerződés kezdete/ vége, kaució összege.
+            //Mérőóra állások: Víz, gáz, villany(havi bontásban).
+            //Költségek: Közös költség, javítási alap, biztosítás.
+            //Emlékeztető: Mikor esedékes a következő gázkészülék - felülvizsgálat.
+            Console.WriteLine("Ingatlanok!");
+        }
+    }
+}
