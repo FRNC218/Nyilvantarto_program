1be1adb7d1a4dd4bcc4b93dc31f28f340bffcad3
GalPeter24 <galpeti20083@gmail.com>
2026-03-20 10:01:03 +0100

    class_kiszervezés

--- nyilvantartas/nyilvantartas/Program.cs
+++ nyilvantartas/nyilvantartas/Program.cs
@@ -7,21 +7,7 @@
     namespace IngatlanNyilvantarto
     {
         // 1. Az Ingatlan osztály definiálása
-        public class Ingatlan
-        {
-            public string Cim { get; set; }
-            public double Meret { get; set; } // m2
-            public string BerloNeve { get; set; }
-            public int BerletiDij { get; set; }
-            public DateTime SzerzodesVege { get; set; }
-            public bool IsKiadva { get; set; }
-
-            public override string ToString()
-            {
-                string statusz = IsKiadva ? $"Kiadva ({BerloNeve}) - {BerletiDij} Ft/hó" : "SZABAD";
-                return $"{Cim.PadRight(20)} | {Meret} m2 | {statusz} | Lejárat: {SzerzodesVege.ToShortDateString()}";
-            }
-        }
+       
 
         class Program
         {
